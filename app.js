ec0702be678b8c2eaa9cbd8715e3fd0643b4501c
hayden7913 <hayden321@gmail.com>
2016-10-28 11:21:57 -0700

    added render functions

--- app.js
+++ app.js
@@ -66,46 +66,101 @@ var promptTemplate = (
 
 var optionTemplate = (
 		"<div>"+
-"			<input type='radio' name='radio-option' > "+
+"			<input type='radio' class='js-radio' name='radio-option' > "+
 "			<span class='js-option option'>Hiya</span>"+
 "			<span class='feedback'></span>"+
 "		</div>"
 );
 
 
-function renderOption(optText, optID, template, optAttr){
+function renderOption(optText, optId, template, optAttr){
 	var element = $(template);
-	element.find(".js-option").text(optText);
+	element
+		.find(".js-option")
+		.text(optText);
+	element
+		.find(".js-radio")
+		.attr("id", optId)
+
 	return element;
 }
 
 
 function renderProblem(state, probElement, probNum, optAttr){
 	var problemsHTML = state.problems[probNum].choices.map(
-		(option, index) => renderOption(option.text, index, optionTemplate)); //console.log(renderOption(option.text))}
-	
-	console.log(problemsHTML);
+		(option, index) => 
+			renderOption(option.text, index, optionTemplate)); //console.log(renderOption(option.text))}
+
 	probElement.html(problemsHTML);
 }
 
 function renderPrompt(state, promptElement, probNum, template){
 	var element = $(template);
 	var promptText = state.problems[probNum].prompt;	
-	element.find(".js-prompt").text(promptText);
+	element
+		.find(".js-prompt")
+		.text(promptText);
+
 	promptElement.html(element);
+}
+
+function renderFeedback(radioId, probNum, probElement){
+	var correctId = findCorrect(probNum);
+	var correct = radioId == correctId;
+
+	if(correct === false){
+		$(probElement)
+			.find("#"+radioId)
+			.closest("div")
+			.find(".feedback")
+			.text("Incorrect")
+			.css("color", "red");
+	}
+
+	$(probElement)
+			.find("#"+correctId)
+			.closest("div")
+			.find(".feedback")
+			.text("Correct")
+			.css("color", "green");
+	
 }
 
 
 // Event Handlers
+
+function findCorrect(radioId, probNum){
+	var res; 
+	state.problems[probNum].choices.forEach(function (option){
+		// console.log(option.id, radioId);
+		if(option.correct){
+			res = option.id;
+		}
+	});
+	return res; 
+}
+
+
+// function isCorrect(radioId, probNum){
+// 	var res; 
+// 	state.problems[probNum].choices.forEach(function (option){
+// 		// console.log(option.id, radioId);
+// 		if(option.id == radioId){
+// 			res = option.correct;
+// 		}
+// 	});
+// 	return res; 
+// }
+
 
 function handleSubmit(){
 	$(".js-submit").click(function (){
 		$("input").each(function (){
 			if($(this).is(":checked")){
 				console.log("hello");
-				var id = $(this).attr("id")
+				var id = $(this).attr("id");
 				console.log(id);
-				$(this).closest("div").find(".feedback").text("Chosen");
+				//$(this).closest("div").find(".feedback").text("Chosen");
 			}
 		})	
 	});
@@ -119,8 +174,11 @@ function main(){
 	handleSubmit();
 	
 	renderPrompt(state, promptElement, probNum, promptTemplate);
-	renderProblem(state, probElement, probNum);
+	renderProblem(state, probElement, probNum);	
 
+	var id="1";
+	$(probElement).find("#"+id).closest("div").find(".feedback").text("hello")
+	//$("form").find("#"+id).text("hello");
 
 	
 }
--- state.js
+++ state.js
@@ -43,7 +43,7 @@ var state = {
 				},
 				{
 					text: "2x",
-					correct: true,
+					correct: false,
 					id: 2
 				},
 				{
