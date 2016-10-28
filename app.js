6c917d738151f4cf69504787dbda8e6ce01f6469
hayden7913 <hayden321@gmail.com>
2016-10-28 11:53:08 -0700

    render feedback working

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ var optionTemplate = (
 "		</div>"
 );
 
+//Rendering
 
 function renderOption(optText, optId, template, optAttr){
 	var element = $(template);
@@ -85,13 +86,12 @@ function renderOption(optText, optId, template, optAttr){
 	return element;
 }
 
-
-function renderProblem(state, probElement, probNum, optAttr){
+function renderProblem(state, optListElement, probNum, optAttr){
 	var problemsHTML = state.problems[probNum].choices.map(
 		(option, index) => 
 			renderOption(option.text, index, optionTemplate)); //console.log(renderOption(option.text))}
 
-	probElement.html(problemsHTML);
+	optListElement.html(problemsHTML);
 }
 
 function renderPrompt(state, promptElement, probNum, template){
@@ -104,12 +104,22 @@ function renderPrompt(state, promptElement, probNum, template){
 	promptElement.html(element);
 }
 
-function renderFeedback(radioId, probNum, probElement){
-	var correctId = findCorrect(probNum);
+function findCorrect(radioId, probNum){
+	var res;
+	state.problems[probNum].choices.forEach(function (option){
+		if(option.correct){
+			res = option.id;
+		}
+	});
+	return res; 
+}
+
+function renderFeedback(radioId, probNum, optListElement){
+	var correctId = findCorrect(radioId, probNum);
 	var correct = radioId == correctId;
 
 	if(correct === false){
-		$(probElement)
+		$(optListElement)
 			.find("#"+radioId)
 			.closest("div")
 			.find(".feedback")
@@ -117,7 +127,7 @@ function renderFeedback(radioId, probNum, probElement){
 			.css("color", "red");
 	}
 
-	$(probElement)
+	$(optListElement)
 			.find("#"+correctId)
 			.closest("div")
 			.find(".feedback")
@@ -129,56 +139,28 @@ function renderFeedback(radioId, probNum, probElement){
 
 // Event Handlers
 
-function findCorrect(radioId, probNum){
-	var res; 
-	state.problems[probNum].choices.forEach(function (option){
-		// console.log(option.id, radioId);
-		if(option.correct){
-			res = option.id;
-		}
-	});
-	return res; 
-}
 
 
-// function isCorrect(radioId, probNum){
-// 	var res; 
-// 	state.problems[probNum].choices.forEach(function (option){
-// 		// console.log(option.id, radioId);
-// 		if(option.id == radioId){
-// 			res = option.correct;
-// 		}
-// 	});
-// 	return res; 
-// }
-
-
-function handleSubmit(){
+function handleSubmit(probNum, optListElement){
 	$(".js-submit").click(function (){
 		$("input").each(function (){
 			if($(this).is(":checked")){
-				console.log("hello");
 				var id = $(this).attr("id");
-				console.log(id);
-				//$(this).closest("div").find(".feedback").text("Chosen");
+				renderFeedback(id, probNum, optListElement);
+				console.log(probNum);
 			}
 		})	
 	});
 } 
 
 function main(){
-
-	var probElement = $(".js-option-list");
+	var optListElement = $(".js-option-list");
 	var promptElement = $(".js-prompt"); 
 	var probNum = 1;	
-	handleSubmit();
+	handleSubmit(probNum, optListElement);
 	
 	renderPrompt(state, promptElement, probNum, promptTemplate);
-	renderProblem(state, probElement, probNum);	
-
-	var id="1";
-	$(probElement).find("#"+id).closest("div").find(".feedback").text("hello")
-	//$("form").find("#"+id).text("hello");
+	renderProblem(state, optListElement, probNum);	
 
 	
 }
