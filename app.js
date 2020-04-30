5beb4c65cec46f8141fd36520499e808b00c11c6
hayden7913 <hayden321@gmail.com>
2020-04-30 16:47:10 -0700

    Disables next button until problem is submitted

--- app.js
+++ app.js
@@ -1,150 +1,178 @@
 //State
 
 var promptTemplate = (
-	"<div>"+
-		"<span class='js-prompt'></span>"+
-	"</div>"
+  "<div>" +
+  "<span class='js-prompt'></span>" +
+  "</div>"
 );
 
 var optionTemplate = (
-		"<div>"+
-"			<input type='radio' class='js-radio' name='radio-option' required> "+
-"			<span class='js-option option'></span>"+
-"			<span class='feedback'></span>"+
-"		</div>"
+  "<div>" +
+  "			<input type='radio' class='js-radio' name='radio-option' required> " +
+  "			<span class='js-option option'></span>" +
+  "			<span class='feedback'></span>" +
+  "		</div>"
 );
 
-//State modification
+// State modification
 
-function isCorrect(state, radioId){
-	var correctId = findCorrect(radioId, state.currentProblem);
-	return radioId == correctId;
+var toggleNextDisabled;
+function makeToggleNextDisabled(buttonEl) {
+  return function () {
+    buttonEl.prop('disabled', function(i, v) { return !v; });
+  }
 }
 
-function findCorrect(radioId){
-	var res;
-	res = state.problems[state.currentProblem].choices.find(function(option){
-		return option.correct;
-	});
-	return res.id; 
+function isCorrect(state, radioId) {
+  var correctId = findCorrect(radioId, state.currentProblem);
+  return radioId == correctId;
 }
 
-function changeProblem(state){
-	state.currentProblem++;
+function findCorrect(radioId) {
+  var res;
+  res = state.problems[state.currentProblem].choices.find(function (option) {
+    return option.correct;
+  });
+  return res.id;
+}
+
+function changeProblem(state) {
+  state.currentProblem++;
 }
 
 function incrementCorrect(state, id) {
-		if(isCorrect(state, id)){
-			state.numCorrect++;
-		}
+  if (isCorrect(state, id)) {
+    state.numCorrect++;
+  }
 }
 
 //Rendering
 
-function renderOption(optText, optId, template, optAttr){
-	var element = $(template);
-	element
-		.find(".js-option")
-		.text(optText);
-	element
-		.find(".js-radio")
-		.attr("id", optId)
+function renderOption(optText, optId, template, optAttr) {
+  var element = $(template);
+  element
+    .find(".js-option")
+    .text(optText);
+  element
+    .find(".js-radio")
+    .attr("id", optId)
 
-	return element;
+  return element;
 }
 
-function renderProblem(state, optListElement, optAttr){
-	var problemsHTML = state.problems[state.currentProblem].choices.map(
-		(option, index) => 
-			renderOption(option.text, index, optionTemplate)); //console.log(renderOption(option.text))}
+function renderProblem(state, optListElement, optAttr) {
+  var problemsHTML = state.problems[state.currentProblem].choices.map(
+    (option, index) =>
+      renderOption(option.text, index, optionTemplate)); //console.log(renderOption(option.text))}
 
-	optListElement.html(problemsHTML);
+  optListElement.html(problemsHTML);
 }
 
-function renderPrompt(state, promptElement, template){
-	var element = $(template);
-	var promptText = state.problems[state.currentProblem].prompt;	
-	element
-		.find(".js-prompt")
-		.text(promptText);
+function renderPrompt(state, promptElement, template) {
+  var element = $(template);
+  var promptText = state.problems[state.currentProblem].prompt;
+  element
+    .find(".js-prompt")
+    .text(promptText);
 
-	promptElement.html(element);
+  promptElement.html(element);
 }
 
 
-function renderFeedback(state, radioId,  optListElement){
-	var correctId = findCorrect(radioId, state.currentProblem);
-	var correct = isCorrect(state, radioId);
+function renderFeedback(state, radioId, optListElement) {
+  var correctId = findCorrect(radioId, state.currentProblem);
+  var correct = isCorrect(state, radioId);
 
-	if(correct === false){
-		$(optListElement)
-			.find("#"+radioId)
-			.closest("div")
-			.find(".feedback")
-			.text("Incorrect")
-			.css("color", "red");
-	}
+  if (correct === false) {
+    $(optListElement)
+      .find("#" + radioId)
+      .closest("div")
+      .find(".feedback")
+      .text("Incorrect")
+      .css("color", "red");
+  }
 
-	$(optListElement)
-			.find("#"+correctId)
-			.closest("div")
-			.find(".feedback")
-			.text("Correct")
-			.css("color", "green");
-	
-   }
+  $(optListElement)
+    .find("#" + correctId)
+    .closest("div")
+    .find(".feedback")
+    .text("Correct")
+    .css("color", "green");
 
- function renderMetrics(state, currProbElement, numCorrectElement, numAnsweredElement){
- 	currProbElement.text(state.currentProblem + 1);
- 	numCorrectElement.text(state.numCorrect);
- 	numAnsweredElement.text(state.numAnswered);
+}
 
- }
+function renderMetrics(state, currProbElement, numCorrectElement, numAnsweredElement) {
+  currProbElement.text(state.currentProblem + 1);
+  numCorrectElement.text(state.numCorrect);
+  numAnsweredElement.text(state.numAnswered);
+}
 
- function renderAll(
- 	state, promptElement,  promptTemplate, optListElement, currProbElement, numCorrectElement , numAnsweredElement){
-   	renderPrompt(state, promptElement,  promptTemplate);
-	renderProblem(state, optListElement);	
-	renderMetrics(state, currProbElement, numCorrectElement ,numAnsweredElement);
-   }
+function renderAll(
+  state, promptElement, promptTemplate, optListElement, currProbElement, numCorrectElement, numAnsweredElement
+) {
+  renderPrompt(state, promptElement, promptTemplate);
+  renderProblem(state, optListElement);
+  renderMetrics(state, currProbElement, numCorrectElement, numAnsweredElement);
+}
 
 
 // Event Handlers
 
-function handleSubmit(state, optListElement, currProbElement, numCorrectElement, numAnsweredElement){
-	$(".js-submit").click(function (){
-		$("input").each(function (){
-			if($(this).is(":checked")){				
-				var id = $(this).attr("id");
-				state.numAnswered++;
-				incrementCorrect(state, id);
-				renderFeedback(state, id,  optListElement);
-				renderMetrics(state, currProbElement, numCorrectElement, numAnsweredElement)
-			}
-		})
+function handleSubmit(state, optListElement, currProbElement, numCorrectElement, numAnsweredElement) {
+  $(".js-submit").click(function () {
+    $("input").each(function () {
+      if ($(this).is(":checked")) {
+        var id = $(this).attr("id");
+        state.numAnswered++;
+        incrementCorrect(state, id);
+        renderFeedback(state, id, optListElement);
+        renderMetrics(state, currProbElement, numCorrectElement, numAnsweredElement)
+        toggleNextDisabled();
+      }
+    })
 
-	});
+  });
 }
 
-function handleNext(state, promptElement, optListElement, promptTemplate, currProbElement, numCorrectElement, numAnsweredElement){
-	$(".js-next").click(function() {
-		changeProblem(state);
-		renderAll(state, promptElement, promptTemplate, optListElement , currProbElement, numCorrectElement, numAnsweredElement);
-	})
+function handleNext(state, promptElement, optListElement, promptTemplate, currProbElement, numCorrectElement, numAnsweredElement) {
+  $(".js-next").click(function () {
+    changeProblem(state);
+    renderAll(state, promptElement, promptTemplate, optListElement, currProbElement, numCorrectElement, numAnsweredElement);
+    toggleNextDisabled();
+  })
 }
 
-function main(){
-	var optListElement = $(".js-option-list");
-	var promptElement = $(".js-prompt"); 
-	var currProbElement = $(".js-currProb");
-	var numCorrectElement = $(".js-numCorrect");
-	var numAnsweredElement= $(".js-answered");
-	var totalProb = state.problems.length;
-	$(".js-totalProb").text(totalProb);	
-	renderAll(state, promptElement,  promptTemplate, optListElement, currProbElement, numCorrectElement, numAnsweredElement);
-	handleSubmit(state, optListElement, currProbElement, numCorrectElement, numAnsweredElement);
-	handleNext(state, promptElement, optListElement, promptTemplate , currProbElement, numCorrectElement);
 
+function main() {
+  var optListElement = $(".js-option-list");
+  var promptElement = $(".js-prompt");
+  var currProbElement = $(".js-currProb");
+  var numCorrectElement = $(".js-numCorrect");
+  var numAnsweredElement = $(".js-answered");
+  var startButtonElement = $(".js-start-button");
+  var nextButtonElement = $(".js-next");
+  var welcomePage = $(".js-welcome-page");
+  var quizPage = $(".js-quiz-page");
+  var totalProb = state.problems.length;
+  toggleNextDisabled = makeToggleNextDisabled(nextButtonElement);
+  // add welcomePageElement
+  // make handleStart func
+  startButtonElement.click(function () {
+    welcomePage.css("display", "none");
+    quizPage.css("display", "block");
+  });
+
+  // for testing
+    welcomePage.css("display", "none");
+    quizPage.css("display", "block");
+  // for testing
+
+
+
+  $(".js-totalProb").text(totalProb);
+  renderAll(state, promptElement, promptTemplate, optListElement, currProbElement, numCorrectElement, numAnsweredElement);
+  handleSubmit(state, optListElement, currProbElement, numCorrectElement, numAnsweredElement);
+  handleNext(state, promptElement, optListElement, promptTemplate, currProbElement, numCorrectElement, numAnsweredElement);
 }
 
 $(document).ready(main());
--- state.js
+++ state.js
@@ -1,144 +1,141 @@
 var state = {
-	problems: [
-		{
-			prompt: "Find d/dx of x^2",
-			choices: [ 
-				{
-					text: "x^3",
-					correct: false,
-					id: 0
-				},
-				{
-					text: "2x^3",
-					correct: false,
-					id: 1
-				},
-				{
-					text: "2x",
-					correct: true,
-					id: 2
-				},
-				{
-					text: "1/2x",
-					correct: false,
-					id: 3
-				}
+  problems: [
+    {
+      prompt: "Find d/dx of x^2",
+      choices: [
+        {
+          text: "x^3",
+          correct: false,
+          id: 0
+        },
+        {
+          text: "2x^3",
+          correct: false,
+          id: 1
+        },
+        {
+          text: "2x",
+          correct: true,
+          id: 2
+        },
+        {
+          text: "1/2x",
+          correct: false,
+          id: 3
+        }
 
-			] 
-		},
+      ]
+    },
 
-		{
-			prompt: "Integrate x^2",
-			choices: [ 
-				{
-					text: "x^3",
-					correct: false,
-					id: 0
-				},
-				{
-					text: "2x^3",
-					correct: false,
-					id: 1
-				},
-				{
-					text: "2x",
-					correct: false,
-					id: 2
-				},
-				{
-					text: "1/2x",
-					correct: true,
-					id: 3
-				}
+    {
+      prompt: "Integrate x^2",
+      choices: [
+        {
+          text: "x^3",
+          correct: false,
+          id: 0
+        },
+        {
+          text: "2x^3",
+          correct: false,
+          id: 1
+        },
+        {
+          text: "2x",
+          correct: false,
+          id: 2
+        },
+        {
+          text: "1/2x",
+          correct: true,
+          id: 3
+        }
 
-			] 
-		},
+      ]
+    },
 
-		{
-			prompt: "Integrate x^2 from 0 to 1",
-			choices: [ 
-				{
-					text: "1/2",
-					correct: true,
-					id: 0
-				},
-				{
-					text: "1/4",
-					correct: false,
-					id: 1
-				},
-				{
-					text: "2",
-					correct: false,
-					id: 2
-				},
-				{
-					text: "1",
-					correct: false,
-					id: 3
-				}
+    {
+      prompt: "Integrate x^2 from 0 to 1",
+      choices: [
+        {
+          text: "1/2",
+          correct: true,
+          id: 0
+        },
+        {
+          text: "1/4",
+          correct: false,
+          id: 1
+        },
+        {
+          text: "2",
+          correct: false,
+          id: 2
+        },
+        {
+          text: "1",
+          correct: false,
+          id: 3
+        }
 
-			] 
-		},
+      ]
+    },
 
-		{
-			prompt: "Find d/dx x^2y",
-			choices: [ 
-				{
-					text: "x^2",
-					correct: false,
-					id: 0
-				},
-				{
-					text: "2x",
-					correct: false,
-					id: 1
-				},
-				{
-					text: "y",
-					correct: false,
-					id: 2
-				},
-				{
-					text: "2xy",
-					correct: true,
-					id: 3
-				}
+    {
+      prompt: "Find d/dx x^2y",
+      choices: [
+        {
+          text: "x^2",
+          correct: false,
+          id: 0
+        },
+        {
+          text: "2x",
+          correct: false,
+          id: 1
+        },
+        {
+          text: "y",
+          correct: false,
+          id: 2
+        },
+        {
+          text: "2xy",
+          correct: true,
+          id: 3
+        }
 
-			] 
-		},
+      ]
+    },
 
-		{
-			prompt: "Find d/dy x^2y",
-			choices: [ 
-				{
-					text: "x^2",
-					correct: true,
-					id: 0
-				},
-				{
-					text: "2x",
-					correct: false,
-					id: 1
-				},
-				{
-					text: "y",
-					correct: false,
-					id: 2
-				},
-				{
-					text: "2xy",
-					correct: false,
-					id: 3
-				}
+    {
+      prompt: "Find d/dy x^2y",
+      choices: [
+        {
+          text: "x^2",
+          correct: true,
+          id: 0
+        },
+        {
+          text: "2x",
+          correct: false,
+          id: 1
+        },
+        {
+          text: "y",
+          correct: false,
+          id: 2
+        },
+        {
+          text: "2xy",
+          correct: false,
+          id: 3
+        }
 
-			] 
-		}
-	],
-
-	currentProblem: 0,
-
-	numCorrect: 0,
-
-	numAnswered: 0
+      ]
+    }
+  ],
+  currentProblem: 0,
+  numCorrect: 0,
+  numAnswered: 0
 }
